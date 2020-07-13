54257e45b923c213e0d32ccfcacd3315ecf7baf4
srkrishna3384 <srkrishna3384@gmail.com>
2020-07-13 15:54:55 +0530

    Added new files

---
+++ src/test/java/com/rameshsoft/automation/gmail/scripts/SampleTest.java
@@ -0,0 +1,12 @@
+package com.rameshsoft.automation.gmail.scripts;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void demo() {
+		System.out.println("Test1");
+
+	}
+
+}
